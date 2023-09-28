25b081b3bb0c4260f70ed4c73f10299e18c3c9a6
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-09-28 16:25:44 +0300

    creates privateroute at frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './Redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from 'react-router-dom';
+} from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./Assets/styles/bootstrap.custom.css";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-import HomeScreen from './Pages/HomeScreen';
+import HomeScreen from "./Pages/HomeScreen";
 import ProductScreen from "./Pages/ProductScreen";
-import CartScreen from './Pages/CartScreen';
-import LoginScreen from './Pages/LoginScreen';
-import RegisterScreen from './Pages/RegisterScreen';
-import ShippingScreen from './Pages/ShippingScreen';
-
+import CartScreen from "./Pages/CartScreen";
+import LoginScreen from "./Pages/LoginScreen";
+import RegisterScreen from "./Pages/RegisterScreen";
+import ShippingScreen from "./Pages/ShippingScreen";
+import PrivateRoute from "./Components/PrivateRoute";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,17 +30,18 @@ const router = createBrowserRouter(
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
 
-      <Route path="/shipping" element={<ShippingScreen />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-  <RouterProvider router={router} />
-      </Provider>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
-
