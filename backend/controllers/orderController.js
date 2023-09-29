f9e703eb9e8ad344c3d3d7209e9d7dc3f5fa9771
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-09-29 11:09:31 +0300

    Updated addOrder, getmyorders and getOrderById at backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,21 +5,63 @@ import Order from "../models/orderModel.js";
 // @route GET /api/v1/orders
 // @access public
 const addOrderItems = asynchronousHandler(async (req, res) => {
-  res.send("add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((item) => ({
+        ...item,
+        product: item._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc    Get logged in user orders
 // @route   GET /api/v1/myorders
 // @access  Private
 const getMyOrders = asynchronousHandler(async (req, res) => {
-  res.send("get my orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
 // @route   GET /api/v1/orders/:id
 // @access  Private
 const getOrderById = asynchronousHandler(async (req, res) => {
-  res.send("get order by Id");
+  const order = await Order
+  .findById(req.params.id)
+  .populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to paid
@@ -49,5 +91,5 @@ export {
   getOrderById,
   updateOrderToPaid,
   updateOrderToDelivered,
-  getAllOrders
+  getAllOrders,
 };
