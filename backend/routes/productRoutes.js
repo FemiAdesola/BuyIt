fc9fd277f17de023857988bdf11239bfbeb409c5
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-10-03 12:44:04 +0300

    Added product review to productscreen and updated productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,11 +10,14 @@ import {
     deleteProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddlewareHandler.js";
+import { createProductReview } from "../controllers/reviewController.js";
 
 router.route('/').get(getAllProducts).post(protect, admin, createProduct);
 router.route('/:id')
     .get(getProductById)
     .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct);
+
+ router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
--- backend/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { createProductReview } from "../controllers/reviewController.js";
-import { protect, admin } from "../middleware/authMiddlewareHandler.js";
-
-router.route('/').post(protect, createProductReview);
-
-export default router;
